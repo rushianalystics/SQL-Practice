f043f110987a16c2e91ec418e315b7518e2dae08
Rushikesh Shirsath <rushikeshshirsath22@gmail.com>
2026-08-19 17:58:03 +0530

    Update CreateDataBase.sql

--- CreateDataBase.sql
+++ CreateDataBase.sql
@@ -19,3 +19,30 @@ step 4)  insert into maihu (maihu_id , name , age , city , salary , email_id)
          (4,'kaustubh',24,'manchar',45000,'kaustubh24@gmail.com');
 
 step 5)  select * from maihu;
+
+
+       2ND DATABASE 
+
+
+step 1) create database dnynu;
+
+step 2) use dnynu;
+
+step 3) create table rushi(
+        rushi_id int  primary key,
+        name varchar(100) not null,
+        age int not null,
+        city char(50) not null,
+        email_id char(50) unique,
+        education varchar(50) not null,
+        salary char(50) not null
+        );
+
+step 4) insert into rushi (rushi_id , name , age , city ,  email_id, education, salary)
+        values
+        (1,'adi' , 21,'manchar','adi21@gamil.com','be','fresher'),
+        (2,'yash' , 22 , 'pune', 'yash22@gmail.com','te','fresher'),
+        (3,'paju' , 23, 'junner','paju23@gmail.com','data analyst',30000),
+        (4,'kaustubh',24,'manchar','kaustubh24@gmail.com', 'be','fresher');
+
+        select * from rushi;
