bb83724502b74f875b7ca8fd1f7d01487c4cc27c
Rushikesh Shirsath <rushikeshshirsath22@gmail.com>
2026-08-25 20:05:00 +0530

    Update sql.practice 6.sql

--- sql.practice 6.sql
+++ sql.practice 6.sql
@@ -1,26 +1,30 @@
 1) Find the running total of salaries based on emp_id =
 
    select emp_name , salary ,
-   sum(salary)over( order by emp_id) as running_salary
+   sum(salary)over( order by emp_id) 
+   as running_salary
    from employees;
 
 2) Find the running total of salary for each department separately =
 
    select  department , emp_name , salary ,
-   sum(salary)over( partition by department order by emp_id) as running_salary
+   sum(salary)over( partition by department order by emp_id)
+   as running_salary
    from employees;
 
 3) Find the salary rank of every employee within their department =
 
    select  department , emp_name , salary ,
-   RANK()over( partition by department order by SALARY DESC) as SALARY_RANK
+   RANK()over( partition by department order by SALARY DESC) 
+   as SALARY_RANK
    from employees;
 
 4) Find the top 2 highest-paid employees from each department =
 
    SELECT * FROM (
    SELECT *,
-   RANK()OVER(PARTITION BY DEPARTMENT ORDER BY SALARY DESC) AS RNK
+   RANK()OVER(PARTITION BY DEPARTMENT ORDER BY SALARY DESC) 
+   AS RNK
    FROM EMPLOYEES
    )A
    WHERE RNK <= 2;
@@ -29,7 +33,8 @@
 
    SELECT * FROM (
    SELECT EMP_NAME , SALARY ,
-   DENSE_RANK()OVER(PARTITION BY DEPARTMENT ORDER BY SALARY DESC) AS RNK
+   DENSE_RANK()OVER(PARTITION BY DEPARTMENT ORDER BY SALARY DESC) 
+   AS RNK
    FROM EMPLOYEES
    )A
   WHERE RNK = 2;
